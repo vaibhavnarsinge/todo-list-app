1c8cf2a9cdbf12ea47f7989559568f43ac80c4d1
Vaibhav Narsinge <vaibhavnarsinge321@gmail.com>
2023-12-13 13:37:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 80;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+let tasks = [];
+
+app.get('/', (req, res) => {
+  res.render('index', { tasks });
+});
+
+app.post('/add', (req, res) => {
+  const task = req.body.task;
+  tasks.push(task);
+  res.redirect('/');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
